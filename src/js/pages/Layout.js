19a11dc5abbecbb72e7d40938184741f9759b62a
Jorge Rodriguez <rodri0315@gmail.com>
2017-11-26 13:12:20 -0500

    Add page components, setup react-router.

--- src/js/pages/Layout.js
+++ src/js/pages/Layout.js
@@ -1,11 +1,22 @@
 import React from "react";
-
+import { Link } from 'react-router'
 
 
 export default class Layout extends React.Component {
+  navigate() {
+    console.log(this.props);
+    this.props.history.pushState(null,"/");
+  }
+
   render() {
     return (
-    <div>Killer News</div>
+      <div>
+        <h1>Killer News</h1>
+        {this.props.children}
+        <Link to="archives" class="btn btn-danger">Archives</Link>
+        <Link to="settings"><button class="btn btn-success">Settings</button></Link>
+        <button onClick={this.navigate.bind(this)}>Featured</button>
+      </div>
     );
   }
 }
